2309fef0ff20b2462ba379fb48d68ed37bbcc220
christopher pipitone <Chrispipitone123@gmail.com>
2022-01-12 14:35:37 -0500

    improve skeleton

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-    
-    </div>
-  );
+class App extends Component() {
+  render(){
+     return (
+      <div className="App">
+      
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Credit.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Credit extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
