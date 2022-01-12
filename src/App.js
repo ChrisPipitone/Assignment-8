399077ad602f16ed8a86b9c1ea7c9ad6498a7186
christopher pipitone <Chrispipitone123@gmail.com>
2022-01-12 15:46:36 -0500

    added nav bar, touble having header/help text in home.js appearing... it is in the nav bar in the browser dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import { Component } from "react";
+import React, { Component } from "react";
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Navbar from 'react-bootstrap/Navbar'
 
-class App extends Component() {
+import Home from "./components/Home";
+import UserProfile from "./components/UserProfile";
+import Login from "./components/Login";
+import Credit from "./components/Credit";
+import Debit from "./components/Debit";
+
+class App extends Component{
   render(){
      return (
-      <div className="App">
-      
-      </div>
+       
+      <Router>
+         <div className="App">
+           <Navbar bg="dark" variant="dark">
+           <Navbar.Brand>Bank of React</Navbar.Brand>
+
+             <div className="content">
+               <Routes>
+                 <Route exact path = "/" element = {<Home />} />
+                 <Route exact path = "/userProfile" element = {<UserProfile />} />
+                 <Route exact path = "/Login" element = {<Login />} />
+               </Routes>
+
+             </div>
+             
+           </Navbar>
+         </div>
+      </Router>
+     
     );
   }
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react'
+import AccountBalance from './AccountBalance'
+import {Link} from 'react-router-dom'
 
 export default class Home extends Component {
     render() {
         return (
-            <div>
+            <div> 
+                <h1> help</h1>
+                
+                <ul className="nav m-2 justify-content-center">
+                    <Link to ="/login">Login</Link> <br/>
+                    <Link to = "/userProfile">User Profile</Link> <br/>
+                </ul>
                 
             </div>
         )
